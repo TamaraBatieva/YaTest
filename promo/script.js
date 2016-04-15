756b9aadbbed6ef556734b8d313369e580911f4a
Tamara Batieva <batieva.tamara@gmail.com>
2016-04-16 02:04:07 +0300

    add autoPlay to slider, add font, fix line-height

--- promo/script.js
+++ promo/script.js
@@ -1,54 +1,62 @@
-$(document).ready(function() {
- $(".slider").each(function () { // обрабатываем каждый слайдер
-  var obj = $(this);
-  $(obj).append("<div class='nav'></div>");
-  $(obj).find("li").each(function () {
-   $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
-   $(this).addClass("slider"+$(this).index());
-  });
-  $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
- });
+var intervalId;
+var timeoutId;
+
+$(document).ready(function () {
+    setupNavigation();
+    startAutoPlay();
 });
-function sliderJS (obj, sl) { // slider function
- var ul = $(sl).find("ul"); // находим блок
- var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
- var step = $(bl).width(); // ширина объекта
- $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
+
+$(document).on("click", ".slider .nav span", function () {
+    stopAutoPlay();
+    var slideIndex = parseInt($(this).attr("rel")); // узнаем номер
+    goToSlide(slideIndex); // слайдим
+    timeoutId = setTimeout(startAutoPlay, 5000);
+    return false;
+});
+
+function setupNavigation() {
+    var slider = $(".slider");
+    $(slider).append("<div class='nav'></div>");
+    var nav = $(slider).find(".nav");
+    $(slider).find("li").each(function () {
+        var liIndex = $(this).index();
+        $(nav).append("<span rel='" + liIndex + "'></span>"); // добавляем блок навигации
+        $(this).addClass("slider" + liIndex);
+    });
+	
+    $(slider).find("span").first().addClass("on"); // делаем активным первый элемент меню
 }
-$(document).on("click", ".slider .nav span", function() { // slider click navigate
- var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
- $(sl).find("span").removeClass("on"); // убираем активный элемент
- $(this).addClass("on"); // делаем активным текущий
- var obj = $(this).attr("rel"); // узнаем его номер
- sliderJS(obj, sl); // слайдим
- return false;
-});
 
-// var slidesCount;
-// $(document).ready(function() {
-// 	var counter;
-// 	timerStart(2000, function() {
-// 		counter++;
-// 		if (counter > slidesCount) {
-// 			counter = 1;
-// 		}
-// 		sliderJs( $(this).closest(".slider"))
-// 	});
+function startAutoPlay() {
+    intervalId = setInterval(goNextSlide, 3000);
+}
 
-// 	});
-var counter = 1;
-$(document).ready(function() {
-	var carouselDiv = document.getElementById('sliderDiv');
-	var slidesCount = carouselDiv.getElementsByTagName('li').length;
-	var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
- $(sl).find("span").removeClass("on"); // убираем активный элемент
- $(this).addClass("on"); // делаем активным текущий
- var obj = $(this).attr("rel"); // узнаем его номер
- sliderJS(obj, sl); // слайдим
- return false;
-	setInterval(sliderJS, 1000);
-	counter++;
-	if (counter > slidesCount) {
- 			counter = 1;
-		}
-});
+function stopAutoPlay() {
+    clearInterval(intervalId);
+    clearTimeout(timeoutId);
+}
+
+function getActiveSlideIndex() {
+    return parseInt($(".on").attr("rel"));
+}
+
+function goNextSlide() {
+    var nextSlideIndex = getActiveSlideIndex() + 1;
+    goToSlide(nextSlideIndex);
+}
+
+function goToSlide(slideIndex) {
+    var slider =  $(".slider");
+
+    if (slideIndex >= $(slider).find("li").length) {
+        slideIndex = 0;
+    }
+
+    $(".on").removeClass("on");
+    $("span[rel=" + slideIndex + "]").addClass("on");
+
+    var ul = $(slider).find("ul");
+    var slide = $(slider).find("li").first();
+    var step = $(slide).width();
+    $(ul).animate({marginLeft: "-" + step * slideIndex}, 500);
+}
